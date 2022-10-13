6399330faefb87e842724ac7451bf9938af47072
mohammedshahab <iraqapple435@gmail.com>
2022-10-13 14:55:29 +0300

    write functions

---
+++ main.py
@@ -0,0 +1,8 @@
+x=int(input("enter number X"))
+y=int(input("enter number Y"))
+
+if x>y and x>=10:
+    print("x is bigger and is  bigger 10")
+elif x>y and x<=10:
+    print("x is bigger and not bigger 10")
+
